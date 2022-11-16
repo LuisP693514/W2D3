e0afb5815afaa48c1cca7d34099409144ba3b80b
Luis <perezluis167@gmail.com>
2022-11-16 15:32:54 -0500

    Added empty? and place_mark methods

--- tic_tac_toe_v1/board.rb
+++ tic_tac_toe_v1/board.rb
@@ -18,8 +18,26 @@ class Board
 
     end
 
+    def empty?(pos)
 
+        @grid[pos[0]][pos[1]] == "_"
 
+    end
+
+    def place_mark(pos, mark)
+
+        if self.valid?(pos)
+            if self.empty?(pos)
+                @grid[pos[0]][pos[1]] = mark
+
+            else
+                raise RuntimeError.new("Position is not empty!")
+            end
+
+        else
+            raise RuntimeError.new("Position is not valid! (Top left is [0,0], bottom right is [2,2])")
+        end
+    end
 end
 
 b = Board.new
@@ -27,3 +45,9 @@ b = Board.new
 # p b.valid?([1,4]) # false
 # p b.valid?([2,2]) # true
 # p b.valid?([-1, 2]) # false
+
+# p b.empty?([1,1]) #true
+
+# p b.place_mark([1,4], "X") # Invalid spot error
+# b.place_mark([0,0], "x")
+# p b.place_mark([0,0], "o") # Invalid not empty
